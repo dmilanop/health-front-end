c59d92c6a38d5cbbb94483d2a37a77464f8ae0c9
ArausyCorzo <90804785+ArausyCorzo@users.noreply.github.com>
2022-01-22 16:19:05 -0400

    Ya arregle el input

--- src/js/component/Navbar.jsx
+++ src/js/component/Navbar.jsx
@@ -31,35 +31,36 @@ const Navbar = () => {
 														<div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
 													</div>
 													<div className="mb-3">
-														<label for="exampleInputPassword1" className="form-label">Password</label>
-														<input type="password" className="form-control" id="exampleInputPassword1"/>
+														<label for="passwordInputText1" className="form-label">Password</label>
+														<input type="text" className="form-control" id="exampleInputText1" aria-describedby="TextdHelp"/>
 													</div>
 													<div className="mb-3">
-														<label for="exampleInputPassword1" className="form-label">Name</label>
-														<input type="password" className="form-control" id="exampleInputPassword1"/>
+														<label for="exampleInputName1" className="form-label">Name</label>
+														<input type="text" className="form-control" id="exampleInputName1" aria-describedby="nameHelp"/>
 													</div>
 													<div className="mb-3">
-														<label for="exampleInputPassword1" className="form-label">Lastname</label>
-														<input type="password" className="form-control" id="exampleInputPassword1"/>
+														<label for="exampleInputLasame1" className="form-label">Lastname</label>
+														<input type="text" className="form-control" id="exampleInputLastname1" aria-describedby="lastnameHelp"/>
 													</div>
 													<div className="mb-3">
-														<label for="exampleInputPassword1" className="form-label">Gender</label>
-														<input type="password" className="form-control" id="exampleInputPassword1"/>
-														<div id="emailHelp" className="form-text">Response female/male</div>
+														<label for="exampleInputName1" className="form-label">Gender</label>
+														<input type="text" className="form-control" id="exampleInputGender1" aria-describedby="GenderHelp"/>
+														<div id="genderHelp" className="form-text">Response female/male</div>
 													</div>
 													<div className="mb-3">
-														<label for="exampleInputPassword1" className="form-label">Date of birth</label>
-														<input type="password" className="form-control" id="exampleInputPassword1"/>
+														<label for="exampleInputDate1" className="form-label">Date of birth</label>
+														<input type="date" className="form-control" id="exampleInputdate1" aria-describedby="DateHelp"/>
+														<div id="dateHelp" className="form-text">Response day/month/year</div>
 													</div>
 													<div className="mb-3">
-														<label for="exampleInputPassword1" className="form-label">¿Do you suffer from any ailment?</label>
-														<input type="password" className="form-control" id="exampleInputPassword1"/>
-														<div id="emailHelp" className="form-text">Response yes/no</div>
+														<label for="exampleInputQuestion1" className="form-label">¿Do you suffer from any ailment?</label>
+														<input type="text" className="form-control" id="exampleInputquestion1" aria-describedby="QuestionHelp"/>
+														<div id="questionHelp" className="form-text">Response yes/no</div>
 													</div>
 													<div className="mb-3">
-														<label for="exampleInputPassword1" className="form-label">¿Do you have some type of physical activity?</label>
-														<input type="password" className="form-control" id="exampleInputPassword1"/>
-														<div id="emailHelp" className="form-text">Response yes/no</div>
+														<label for="exampleInputEx1" className="form-label">¿Do you have some type of physical activity?</label>
+														<input type="text" className="form-control" id="exampleInputEx1"/>
+														<div id="exHelp" className="form-text">Response yes/no</div>
 													</div>
 												</form>
 												
@@ -85,12 +86,12 @@ const Navbar = () => {
 											<div className="modal-body">
 												<div className="mb-3">
 													<label for="exampleInputEmail1" className="form-label">Email address</label>
-													<input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+													<input type="text" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
 													<div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
 												</div>
 												<div className="mb-3">
 													<label for="exampleInputPassword1" className="form-label">Password</label>
-													<input type="password" className="form-control" id="exampleInputPassword1"/>
+													<input type="text" className="form-control" id="exampleInputPassword1"/>
 												</div>
 											</div>
 											<div className="modal-footer">
